0bc00d4ecb30fc4e2231d072e1ec9f0089d2ed00
Manuel Ramirez Coronel <ra_cm@outlook.com>
2022-05-03 16:16:19 -0500

    add a edit with ajax and solve get the id of the row clicked with wp datatables

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -38,4 +38,68 @@ jQuery(document).ready(function ($) {
             }
         })
     });
+
+    $('#formulario-edit').submit(function (e) {
+        e.preventDefault();
+        var id = $('#id').val();
+        var nombre = $('#name_customer').val();
+        var correo = $('#email_customer').val();
+        var estados = $('#estados').val();
+        //La llamada AJAX
+        $.ajax({
+            type: 'POST',
+            url: ajax_object.ajax_url, // Pon aquí tu URL
+            data: {
+                action: 'update_custom_form',
+                id_customer: id,
+                name_customer: nombre,
+                email_customer: correo,
+                estados: estados
+            },
+            beforeSend: function (response) {
+                $('#alerts').addClass('is-info');
+                $('#alerts').show();
+                $('#alerts .message-header').html('Enviando datos...');
+                $('#alerts .message-body').html('Tomara unos segundos...');
+            },
+            success: function (response) {
+                // Actualiza el mensaje con la respuesta
+                $('#alerts').addClass('is-success');
+                $('#alerts').show();
+                $('#alerts .message-header').html('Actualizado');
+                $('#alerts .message-body').html('Tu datos se han actualizado correctamente en la Base de Datos.');
+                console.log(response);
+            },
+            error: function (response) {
+                 // Actualiza el mensaje con la respuesta
+                 $('#alerts').addClass('is-danger');
+                 $('#alerts').show();
+                 $('#alerts .message-header').html('Error');
+                 $('#alerts .message-body').html('Ha ocuriido un error al enviar los datos a la Base de Datos, intentelo nuevamente.');
+            }
+        })
+    });
+
+    /** DataTables */
+    var table = $('#example').DataTable({
+        rowId: [0],
+        "columnDefs": [ {
+            "targets": -1,
+            "data": null,
+            "defaultContent": "<button>Editar</button>"
+        }]
+    });
+    $('#example tbody').on( 'click', 'button', function () {
+        var data = table.row( $(this).parents('tr') ).data();
+        alert("El ID es " + data[0]);
+        window.location.href = '/edit?id='+data[0];
+    } );
+    
+    /**
+     * 
+    $('#example').on( 'click', 'tr', function () {
+        var id = table.row( this ).id();
+        alert( 'Clicked row id '+id );
+    } );
+    */
 });
--- crud-with-ajax.php
+++ crud-with-ajax.php
@@ -5,48 +5,66 @@ Plugin Name: CRUD with AJAX
 function crud_enqueue_scripts()
 {
     wp_register_style('bulma-css', 'https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css', '1.0', 'all');
-    wp_enqueue_script('custom-js', plugin_dir_url(__FILE__) . '/assets/js/custom.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('custom-js', plugin_dir_url(__FILE__) . 'assets/js/custom.js', array('jquery'), '1.0', true);
     wp_localize_script('custom-js', 'ajax_object', array(
         'ajax_url' => admin_url('admin-ajax.php'),
     ));
+    /** Add a Datatables */
+    wp_enqueue_style('datatables', 'https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css', '1.0', 'all');
+    wp_enqueue_script('datatables', 'https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('datatables-2', 'https://cdn.datatables.net/1.11.5/js/dataTables.bulma.min.js', array('jquery'), '1.0', true);
+    wp_enqueue_style('datatables-bulma', 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css', '1.0', 'all');
+    wp_enqueue_style('datatables-bulma-2', 'https://cdn.datatables.net/1.11.5/css/dataTables.bulma.min.css', '1.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'crud_enqueue_scripts');
 
 add_shortcode('formulario', 'crud_formulario');
 function crud_formulario()
 {
+
+    global $wpdb;
+
+    $id = $_GET['id'];
+    echo $id;
+
+    $table_name = $wpdb->prefix . 'crud';
+
+    $consulta = $wpdb->get_results("SELECT * FROM $table_name WHERE ID = $id ");
+    echo var_dump($consulta);
+
     wp_enqueue_style('bulma-css');
     ?>
 <form id="formulario" name="formulario" method="POST" action="">
-<div class="field">
-  <label class="label">Nombre</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="e.g Alex Smith" name="name_customer" id="name_customer">
+  <div class="field">
+    <label class="label">Nombre</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="e.g Alex Smith" name="name_customer" id="name_customer">
+    </div>
   </div>
-</div>
 
-<div class="field">
-  <label class="label">Email</label>
-  <div class="control">
-    <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" name="email_customer" id="email_customer">
+  <div class="field">
+    <label class="label">Email</label>
+    <div class="control">
+      <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" name="email_customer"
+        id="email_customer">
+    </div>
   </div>
-</div>
 
-<div class="field">
-  <label class="label">Ciudad</label>
-  <div class="control">
-    <div class="select">
-    <select name="estados" id="estados">
-        <option selected="selected" value="">-- Seleciona una Ciudad --</option>
-        <option value="SLP">San Luis Potosí</option>
-        <option value="QRO">Queretaro</option>
-    </select>
-  </div>
-</div>
-<br>
-<div class="control">
-  <button type="submit" class="button is-primary">Enviar</button>
-</div>
+  <div class="field">
+    <label class="label">Ciudad</label>
+    <div class="control">
+      <div class="select">
+        <select name="estados" id="estados">
+          <option selected="selected" value="">-- Seleciona una Ciudad --</option>
+          <option value="SLP">San Luis Potosí</option>
+          <option value="QRO">Queretaro</option>
+        </select>
+      </div>
+    </div>
+    <br>
+    <div class="control">
+      <button type="submit" class="button is-primary">Enviar</button>
+    </div>
 </form>
 <br>
 <article id="alerts" class="message">
@@ -57,6 +75,78 @@ function crud_formulario()
 </article>
 <?php
 }
+
+function crud_edit()
+{
+    global $wpdb;
+
+    $id = $_GET['id'];
+    echo $id;
+
+    $table_name = $wpdb->prefix . 'crud';
+
+    $consulta = $wpdb->get_results("SELECT * FROM $table_name WHERE ID = $id ");
+
+    foreach ($consulta as $data) {
+        # code...
+        ?>
+<form id="formulario-edit" name="formulario-edit" method="POST" action="">
+  <input type="number" hidden id="id" value="<?php echo $id; ?>">
+  <div class="field">
+    <label class="label">Nombre</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="e.g Alex Smith" name="name_customer" id="name_customer"
+        value="<?php echo $data->nombre; ?>">
+    </div>
+  </div>
+
+  <div class="field">
+    <label class="label">Email</label>
+    <div class="control">
+      <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" name="email_customer" id="email_customer"
+        value="<?php echo $data->email; ?>">
+    </div>
+  </div>
+
+  <div class="field">
+    <label class="label">Ciudad</label>
+    <div class="control">
+      <div class="select">
+        <select name="estados" id="estados">
+          <?php
+switch ($data->ciudad) {
+            case 'SLP':
+                # code...
+                echo '<option value="SLP">San Luis Potosí</option>';
+                break;
+            case 'QRO':
+                echo '<option value="QRO">Queretaro</option>';
+                break;
+            default:
+                # code...
+                break;
+        }
+        ?>
+        </select>
+      </div>
+    </div>
+    <br>
+    <div class="control">
+      <button type="submit" class="button is-primary">Enviar</button>
+    </div>
+</form>
+<br>
+<article id="alerts" class="message">
+  <div class="message-header">
+  </div>
+  <div class="message-body">
+  </div>
+</article>
+<?php
+}
+}
+
+add_shortcode('formulario-editar', 'crud_edit');
 
 /**
  * Funcion que captura los valores de una
@@ -89,6 +179,40 @@ function my_save_custom_form()
 add_action('wp_ajax_nopriv_my_save_custom_form', 'my_save_custom_form'); // Para usuarios no logueados
 add_action('wp_ajax_my_save_custom_form', 'my_save_custom_form'); // Para usuarios logueados
 
+/**
+ * Update a Record
+ */
+
+function update_custom_form()
+{
+// Nuestro código de manipulación de los datos
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'crud';
+
+    $id_customer = $_POST['id_customer'];
+    $name_customer = $_POST['name_customer'];
+    $email_customer = $_POST['email_customer'];
+    $city_customer = $_POST['estados'];
+
+    $wpdb->update(
+        $table_name,
+        array(
+            'nombre' => $name_customer,
+            'email' => $email_customer,
+            'ciudad' => $city_customer,
+        ),
+        array(
+          'ID' => $id_customer
+        )
+    );
+
+//wp_redirect(site_url('/')); // <-- here goes address of site that user should be redirected after submitting that form
+    wp_die();
+}
+add_action('wp_ajax_nopriv_update_custom_form', 'update_custom_form'); // Para usuarios no logueados
+add_action('wp_ajax_update_custom_form', 'update_custom_form'); // Para usuarios logueados
+
 global $crud_db_version;
 $crud_db_version = '1.0';
 
@@ -114,3 +238,50 @@ function crud_install()
     add_option('crud_db_version', $crud_db_version);
 }
 register_activation_hook(__FILE__, 'crud_install');
+
+function crud_read_results()
+{
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'crud';
+
+    $registros = $wpdb->get_results("SELECT  * FROM $table_name");
+
+    ?>
+<table id="example" class="table is-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Nombre</th>
+      <th>Correo Electronico</th>
+      <th>Ciudad</th>
+      <th>Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+foreach ($registros as $data) {
+        echo "<tr>";
+        echo "<td>" . $data->id . "</td>";
+        echo "<td>" . $data->nombre . "</td>";
+        echo "<td>" . $data->email . "</td>";
+        echo "<td>" . $data->ciudad . "</td>";
+        echo "<td></td>";
+        echo "</tr>";
+    }
+    ?>
+  </tbody>
+  <tfoot>
+    <tr>
+      <th>ID</th>
+      <th>Nombre</th>
+      <th>Correo Electronico</th>
+      <th>Ciudad</th>
+      <th>Opciones</th>
+    </tr>
+  </tfoot>
+</table>
+<?php
+
+}
+add_shortcode('form_select', 'crud_read_results');
